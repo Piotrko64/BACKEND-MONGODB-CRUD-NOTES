cfc306fc53f217ae45305c88f2181cd7c3429810
Piotr <Piotrko64@gmail.com>
2021-11-01 20:14:16 +0100

    first action and config

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app=express();
 
-app.get('/', (req,res)=>{
-    res.send('<h1>Server is active!</h1>')
-});
+const {port} = require('./config')
+// routes
+const apiRouter = require("./routes/api");
+app.use('/', apiRouter);
 
-const port = 3000;
 
+
+// server
 app.listen(3000, ()=>{
     console.log("Server is listening on..."+port)
 });
